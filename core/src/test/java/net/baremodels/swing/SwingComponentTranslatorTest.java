05516e3af6fd0f833fc5ced73937a36fa7f9dc1f
Curt Cox <curtcox@gmail.com>
2013-09-04 08:01:25 -0500

    Added container support

--- core/src/test/java/net/baremodels/swing/SwingComponentTranslatorTest.java
+++ core/src/test/java/net/baremodels/swing/SwingComponentTranslatorTest.java
@@ -1,5 +1,6 @@
 package net.baremodels.swing;
 
+import net.baremodels.ui.SimpleUIContainer;
 import net.baremodels.ui.UIButton;
 import net.baremodels.ui.UIComponent;
 import org.junit.Test;
@@ -16,6 +17,26 @@ public class SwingComponentTranslatorTest {
     @Test
     public void button() {
         test(new UIButton("a"),JButton.class,"a");
+    }
+
+    @Test
+    public void container_with_two_buttons() {
+        UIComponent one = new UIButton("one");
+        UIComponent two = new UIButton("two");
+        UIComponent ui = new SimpleUIContainer("c",one,two);
+
+        JComponent actual = testObject.translate(ui);
+
+        assertEquals(2,actual.getComponentCount());
+        assertEquals("c",actual.getName());
+
+        JButton buttonOne = (JButton) actual.getComponent(0);
+        assertEquals("one",buttonOne.getName());
+        assertEquals("one",buttonOne.getText());
+
+        JButton buttonTwo = (JButton) actual.getComponent(1);
+        assertEquals("two",buttonTwo.getName());
+        assertEquals("two",buttonTwo.getText());
     }
 
     @Test
