c6845164b9fbab599c805435dfb8479186d4237b
Curt Cox <curtcox@gmail.com>
2013-12-15 09:02:29 -0600

    Renamed ModelContext -> NavigationContext

---
+++ core/src/test/java/net/baremodels/model/NavigationContextTest.java
@@ -0,0 +1,11 @@
+package net.baremodels.model;
+
+import org.junit.Test;
+
+public class NavigationContextTest {
+
+    @Test
+    public void can_create() {
+        new NavigationContext();
+    }
+}
